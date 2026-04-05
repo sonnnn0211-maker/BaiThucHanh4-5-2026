bfee005e7fffaac43b2ef3846343129ee603b320
Nguyen Ngoc Son <sonnnn0211@gmail.com>
2026-04-05 13:15:52 +0700

    install Filament and configure admin panel

---
+++ database/migrations/2026_04_05_032905_create_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('sv123_categories', function (Blueprint $table) {
+        $table->id();
+        $table->string('name')->unique(); // unique
+        $table->string('slug');
+        $table->text('description')->nullable();
+        $table->boolean('is_visible')->default(true);
+        $table->timestamps();
+    });
+}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
